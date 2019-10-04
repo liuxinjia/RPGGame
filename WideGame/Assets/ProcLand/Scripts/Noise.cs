539d1d0e9f468ad4b3a69b2b2f15b13571b118c1
liuxinjia <liux4989@gmail.com>
2019-10-04 17:44:02 +0800

    Update Noise.cs
    Issuse 5

--- WideGame/Assets/ProcLand/Scripts/Noise.cs
+++ WideGame/Assets/ProcLand/Scripts/Noise.cs
@@ -1,17 +1,17 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿using System.Collections;
+using UnityEngine;
 
 public static class Noise {
 
-	public static float[,] GenerateNoiseMap(int mapWidth, int mapHeight, int seed, float scale, int octaves, float persistance, float lacunarity, Vector2 offset) {
-		float[,] noiseMap = new float[mapWidth,mapHeight];
+	public static float[, ] GenerateNoiseMap (int mapWidth, int mapHeight, int seed, float scale, int octaves, float persistance, float lacunarity, Vector2 offset) {
+		float[, ] noiseMap = new float[mapWidth, mapHeight];
 
 		System.Random prng = new System.Random (seed);
 		Vector2[] octaveOffsets = new Vector2[octaves];
 		for (int i = 0; i < octaves; i++) {
 			float offsetX = prng.Next (-100000, 100000) + offset.x;
 			float offsetY = prng.Next (-100000, 100000) + offset.y;
-			octaveOffsets [i] = new Vector2 (offsetX, offsetY);
+			octaveOffsets[i] = new Vector2 (offsetX, offsetY);
 		}
 
 		if (scale <= 0) {
@@ -21,24 +21,23 @@ public static class Noise {
 		float maxNoiseHeight = float.MinValue;
 		float minNoiseHeight = float.MaxValue;
 
-        //always zoom towards the center
+		//always zoom towards the center
 		float halfWidth = mapWidth / 2f;
 		float halfHeight = mapHeight / 2f;
 
-
 		for (int y = 0; y < mapHeight; y++) {
 			for (int x = 0; x < mapWidth; x++) {
-		
+
 				float amplitude = 1;
 				float frequency = 1;
 				float noiseHeight = 0;
 
 				for (int i = 0; i < octaves; i++) {
-					float sampleX = (x-halfWidth) / scale * frequency + octaveOffsets[i].x;
-					float sampleY = (y-halfHeight) / scale * frequency + octaveOffsets[i].y;
+					float sampleX = (x - halfWidth + octaveOffsets[i].x) / scale * frequency;
+					float sampleY = (y - halfHeight - octaveOffsets[i].y) / scale * frequency;
 
-                    //by default, perlinNoise returns range(0,1)
-                    //change it for decreasing in y axis
+					//by default, perlinNoise returns range(0,1)
+					//change it for decreasing in y axis
 					float perlinValue = Mathf.PerlinNoise (sampleX, sampleY) * 2 - 1;
 					noiseHeight += perlinValue * amplitude;
 
@@ -51,14 +50,14 @@ public static class Noise {
 				} else if (noiseHeight < minNoiseHeight) {
 					minNoiseHeight = noiseHeight;
 				}
-				noiseMap [x, y] = noiseHeight;
+				noiseMap[x, y] = noiseHeight;
 			}
 		}
 
-        //After increasing, keep the noiseMapValue in Range(0,1)
+		//After increasing, keep the noiseMapValue in Range(0,1)
 		for (int y = 0; y < mapHeight; y++) {
 			for (int x = 0; x < mapWidth; x++) {
-				noiseMap [x, y] = Mathf.InverseLerp (minNoiseHeight, maxNoiseHeight, noiseMap [x, y]);
+				noiseMap[x, y] = Mathf.InverseLerp (minNoiseHeight, maxNoiseHeight, noiseMap[x, y]);
 			}
 		}
 
